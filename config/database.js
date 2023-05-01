e73d842d196ba32854f5d431711ad39687fcd218
Angel Soto <angelsoto9597@gmail.com>
2023-04-30 22:40:42 -0400

    here is my demo-day project

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://angelsoto9597:testing123@clusterdemoday.2dhvcu2.mongodb.net/demo-day?retryWrites=true&w=majority', 
+    'dbName': 'demo-day'
+};
